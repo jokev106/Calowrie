9a7812344c7bcd4886fc7f0b772a9df24fd9a069
jokev106 <kevinmarsiano@gmail.com>
2021-12-14 10:15:21 +0700

    Update pages.dart

--- lib/views/pages/pages.dart
+++ lib/views/pages/pages.dart
@@ -1,14 +1,16 @@
 import 'dart:ui';
 
+import 'package:calowrie/services/services.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-// part 'SplashPages.dart';
-part 'login_pages.dart';
 
+part 'splash_screen.dart';
+part 'login_pages.dart';
 part 'list_menu.dart';
 part 'register_pages.dart';
 part 'menu_detail.dart';
