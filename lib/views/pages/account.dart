8bd760a2be17191bbc9d6b4c1140754f26b949fd
pjulianyehuda <62707368+pjulianyehuda@users.noreply.github.com>
2022-01-03 10:00:33 +0700

    Tampilan Page User Account

--- lib/views/pages/account.dart
+++ lib/views/pages/account.dart
@@ -1,54 +1,247 @@
 part of 'pages.dart';
+import 'package:flutter/material.dart'
+//  show BorderRadius, BoxFit, BuildContext, Card, Clip, ClipRRect, Color, Colors, Column, Divider, EdgeInsetsDirectional, Expanded, FontWeight, GlobalKey, Image, Key, MainAxisAlignment, MainAxisSize, Padding, RoundedRectangleBorder, Row, Scaffold, ScaffoldState, SingleChildScrollView, State, StatefulWidget, Text, TextStyle, Widget;
+ 
 
-class MyAccount extends StatefulWidget {
-  const MyAccount({ Key? key }) : super(key: key);
+
+class AccountWidget extends StatefulWidget {
+  const AccountWidget({Key key}) : super(key: key);
 
   @override
-  _MyAccountState createState() => _MyAccountState();
+  _AccountWidgetState createState() => _AccountWidgetState();
 }
 
-class _MyAccountState extends State<MyAccount> {
-
-  bool isLoading = false;
+class _AccountWidgetState extends State<AccountWidget> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(     
-      body: Stack(
-        children: [Container(
-          alignment: Alignment.center,
-          child: ElevatedButton.icon(
-                    onPressed: () async {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      await AuthServices.signOut().then((value) {
-                        if (value) {
-                          setState(() {
-                            isLoading = false;
-                          });
-                          AcitivityServices.showToast(
-                              "Logout success", Colors.greenAccent);
-                          Navigator.pushReplacementNamed(
-                              context, Login.routeName);
-                        } else {
-                          setState(() {
-                            isLoading = false;
-                          });
-                          AcitivityServices.showToast("Logout Failed", Colors.redAccent);
-                        }
-                      });
-                    },
-                    icon: const Icon(Icons.logout),
-                    label: const Text("Logout"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.grey[400],
-                      elevation: 0,
-                    )),
-          ),
-          isLoading == true ? AcitivityServices.loadings()
-            : Container()
-        ]
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Color(0xFF262D34),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 70, 0, 0),
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                color: Color(0xFF4B39EF),
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'assets/images/Mia_Deaven.jpg',
+                      width: 90,
+                      height: 90,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                              child: Text(
+                                '[User Name]',
+                                style: TextStyle(
+                                  fontFamily: 'Lexend Deca',
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                              child: Text(
+                                'User.name@domainname.com',
+                                style: TextStyle(
+                                  fontFamily: 'Lexend Deca',
+                                  color: Color(0xFF95A1AC),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1,
+              thickness: 2,
+              indent: 16,
+              endIndent: 16,
+              color: Color(0x250D9BF1),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(24, 4, 0, 8),
+                        child: Text(
+                          'User Bio',
+                          style: TextStyle(
+                            fontFamily: 'Lexend Deca',
+                            color: Color(0xFF95A1AC),
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                          child: Text(
+                            'TEXT',
+                            style: TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(24, 4, 0, 8),
+                        child: Text(
+                          'Height',
+                          style: TextStyle(
+                            fontFamily: 'Lexend Deca',
+                            color: Color(0xFF95A1AC),
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                          child: Text(
+                            '178cm',
+                            style: TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 12),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(24, 4, 0, 8),
+                        child: Text(
+                          'Weight',
+                          style: TextStyle(
+                            fontFamily: 'Lexend Deca',
+                            color: Color(0xFF95A1AC),
+                            fontSize: 14,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                          child: Text(
+                            '78 kg',
+                            style: TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
