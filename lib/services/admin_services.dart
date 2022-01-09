b1b5793884103285cadca609bd34a8a8f83bc4cc
jokev106 <kevinmarsiano@gmail.com>
2022-01-09 22:09:33 +0700

    Update admin_services.dart

--- lib/services/admin_services.dart
+++ lib/services/admin_services.dart
@@ -61,70 +61,17 @@ class AdminServices {
     return hsl;
   }
 
-<<<<<<< Updated upstream
   static Future<bool> editMenu(Menus menus) async {
     await Firebase.initializeApp();
     await menuCollection.doc(menuDocument!.id).update({
-=======
-  static Future<bool> updateItem({
-  required String docId,
-  required Menus menus,
-  required PickedFile imgFile,
-}) async {
-  DocumentReference documentReferencer =
-     menuCollection.doc(menuCollection.id).collection('menu').doc(docId);
-      String dateNow = AcitivityServices.dateNow();
-    Map<String, dynamic> data = <String, dynamic>{
-      'MenuUid': menus.uid,
->>>>>>> Stashed changes
       'MenuName': menus.name,
       'MenuCalorie': menus.calorie,
       'MenuTime': menus.time,
       'MenuType': menus.type,
       'MenuIngredients': menus.ingredients,
-<<<<<<< Updated upstream
       'MenuStep': menus.steps,
     });
 
     return true;
   }
-=======
-      'MenuSteps': menus.steps,
-      'MenuFile': menus.image,
-      'CreatedAt': dateNow,
-      'UpdatedAt': dateNow,
-    };
-
-    await documentReferencer
-      .update(data)
-      .whenComplete(() => print("Note item updated in the database"))
-      .catchError((e) => print(e));
-
-
-    if (menuDocument != null) {
-      ref = FirebaseStorage.instance
-          .ref()
-          .child("images")
-          .child(menuDocument!.id + ".png");
-      uploadTask = ref!.putFile(File(imgFile.path));
-
-      await uploadTask!.whenComplete(() => ref!.getDownloadURL().then(
-            (value) => imgUrl = value,
-          ));
-
-      menuCollection.doc(menuDocument!.id).update({
-        'MenuUid': menuDocument!.id,
-        'MenuFile': imgUrl,
-      });
-      return true;
-    } else {
-      return false;
-    }
-
-
-}
-static Future<DocumentSnapshot> getMenu(String id)async {
-  return await menuCollection.doc(id).get();
->>>>>>> Stashed changes
-}
 }
