93aed4bb7daf5243231046a70e626726199f628d
vinchrist21 <62634885+vinchrist21@users.noreply.github.com>
2022-01-10 18:29:12 +0700

    delete

--- android/app/src/main/kotlin/com/example/calowrie/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.calowrie
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/model/models.dart
+++
@@ -1,5 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-
-part 'users.dart';
-part 'menus.dart';
